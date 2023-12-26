43d1643cfc60cc2d6b08865c5504e89539c2d898
Radex <radexgamer13@gmail.com>
2023-12-26 17:41:09 -0400

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,24 @@
+//this js is a simplificade code to export
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
